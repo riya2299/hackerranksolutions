9b0a6500a0daa36f61c93d59d669ae3576be37cd
Riya <65081420+riya2299@users.noreply.github.com>
2020-05-30 21:07:01 +0530

    Halloween Sale hackerrrank solution
    ..in python

---
+++ halloweensale.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the howManyGames function below.
+def howManyGames(p, d, m, s):
+    c=p
+    l=[]
+    while(sum(l)<=s):
+        if c>m:
+            l.append(c)
+            c=c-d
+        if c<=m:
+            l.append(m)
+    print(l)
+    return (len(l)-1)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    pdms = input().split()
+
+    p = int(pdms[0])
+
+    d = int(pdms[1])
+
+    m = int(pdms[2])
+
+    s = int(pdms[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
